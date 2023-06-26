d84101146c82676510ccf2dd0fded6fcb5c57652
João-Medina <jvmedina2003@gmail.com>
2023-06-25 21:15:45 -0300

    Create 008-promises.js

---
+++ 008-promises.js
@@ -0,0 +1,20 @@
+/*
+function soma() {
+    let soma = 1 + 1
+    if(soma == 2) {
+        sucessCallback()
+    } else {
+        errorCallback()
+    }
+}
+
+function sucessCallback() {
+    console.log('Yeah! Number 2')
+}
+
+function errorCallback() {
+    console.log('ERROO')
+}
+
+soma()
+*/
