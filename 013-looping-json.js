29198bfed0b5d97fc889c049cfcf7e3bfe30196a
João-Medina <jvmedina2003@gmail.com>
2023-08-02 18:49:30 -0300

    Create 013-looping-json.js

---
+++ 013-looping-json.js
@@ -0,0 +1,15 @@
+const array = [
+    data.nome,
+    data.cpf,
+    data.endereco
+]
+
+let newArray = []
+
+data.processoSeletivo.forEach(element => {
+    console.log(element)
+});
+
+console.log(newArray)
+
+
